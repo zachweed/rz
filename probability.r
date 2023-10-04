9bae56842c600ea4d83bc1f5f2518db6ac49e028
zachw-stack <1937294+zachw-stack@users.noreply.github.com>
2023-10-03 20:33:41 -0400

    Update probability.r

--- probability.r
+++ probability.r
@@ -1,15 +1,51 @@
+# Independence Rule
+# @return What is probability of independent events with two outcomes?
+# @param List[Vector[Numeric, Numeric]] probability pairs, where
+#        first number is 0 if NOT and 1 otherwise.
+# @begin what_is_probability_of_sequence
+what_is_probability_of_sequence <- function(sequence) {
+  counter = 1
+  for(s in sequence) {
+    if(s[1] == 0) {
+      s = 1 - s[2]
+    } else {
+      s = s[2]
+    }
+    counter = counter * s
+  }
+  counter
+}
+# @end what_is_probability_of_sequence
+what_is_probability_of_sequence(
+  list(
+    c(1, 0.8),
+    c(1, 0.8),
+    c(1, 0.8)
+  )
+)
+
 # @return What is stacked probability of an outcome?
 #         i.e., dependent probability.
 # @param List[Vector] probability pairs
 # @begin what_is_stacked_probability
-stacked_probability_outcome <- function(probabilities) {
+what_is_stacked_probability_outcome <- function(probabilities) {
   counter = 0
   for(p in probabilities) {
     counter = counter + (p[1] * p[2])
   }
   counter
 }
-# @end stacked_probability_outcome
+# @end what_is_stacked_probability
+
+what_is_stacked_probability_outcome(list(
+  # supra-attribute, sub-attribute
+  c(0.9, 0.6),
+  # supra-attribute, sub-attribute
+  c(0.08, 0.75),
+  # supra-attribute, sub-attribute
+  c(0.02, 0.60)
+))
+
 
 # @return What the probability of two mutually exclusive events
 # @param Probability of event A
@@ -18,27 +54,27 @@ stacked_probability_outcome <- function(probabilities) {
 what_is_probability_of_two_mutually_exclusive_events_combined <- function(A, B) {
   A+B
 }
+what_is_probability_of_two_mutually_exclusive_events_combined(0.15, 0.1)
 # @end what_is_probability_of_two_mutually_exclusive_events_combined
 
-# @return What the probability of two independent events is.
+# @return What the probability of two dependent events is.
 # @param Probability of event A
 # @param Probability of event B
-# usage: whatIsProbabilityOfTwoIndependentEvents((1/6), (12/52))
-# @begin what_is_probability_of_two_events
-probability_of_two_independent_events <- function(A, B) {
+# @begin what_is_probability_of_two_dependent_events
+what_is_probability_of_two_dependent_events <- function(A, B) {
   fractions(A * B)
 }
-probability_of_two_independent_events((1/6), (12/52))
-# @end
+what_is_probability_of_two_dependent_events(0.05, 0.42)
+# @end what_is_probability_of_two_events
 
 # @param Probability of an event.
 # @param How many times to multiply the event.
 # @return Likelihood of n events in a row.
 # @begin what_is_probability_of_three_events
-what_is_probability_of_three_events <- function(probability, number_of_events) {
+what_is_probability_of_n_events <- function(probability, number_of_events) {
   probability * number_of_events
 }
-what_is_probability_of_three_events(0.92, 3)
+what_is_probability_of_n_events((1-0.65), 2)
 # @end what_is_probability_of_three_events
 
 # @return What the probability of two independent events is.
@@ -59,13 +95,14 @@ what_is_probability_of_two_independent_events(0.273, 0.403, 0.176)
 # @param A One event's probability.
 # @param B another event's probability.
 # @param C probability of both.
+# @begin what_is_probability_that_neither
 what_is_probability_that_neither <- function(A, B, C) {
   1 - ((A + B) - C)
 }
-what_is_probability_that_neither(0.6, 0.3, 0.2)
+what_is_probability_that_neither(0.7, 0.4, 0.3)
+# @end what_is_probability_that_neither
 
-# @begin what_is_probability_given_that
-# make sure to look for both.
+# Conditional Probability Formula.
 # @param probability_of_both First dependent event
 # @param given_what second dependent event
 # @return probability of something given another thing
@@ -74,7 +111,7 @@ what_is_probability_given_that <- function(probability_of_both, probability_of_w
   probability_of_both / probability_of_whats_given
 }
 # @end what_is_probability_given_that
-what_is_probability_given_that(0.48, 0.08)
+what_is_probability_given_that(0.02, 0.05)
 
 
 ###################################
