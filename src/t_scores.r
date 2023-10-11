e3eef9912e06d8d18fb228414ea501fed032aa8f
z-weed <1937294+z-weed@users.noreply.github.com>
2023-10-11 12:02:39 -0400

    Update t_scores.r

--- src/t_scores.r
+++ src/t_scores.r
@@ -1,30 +1,37 @@
-# @begin t_score
-t_score <- function(x_bar=3, mu=3, standard_deviations=3, sample_size=3) {
-  (x_bar - mu)/(standard_deviations/sqrt(sample_size))
-}
-# @end t_score
+library(methods)
 
-# @begin degrees_of_freedom
-df <- function(standard_deviations) {
-  standard_deviations - 1
-}
-degrees_of_freedom <- function(standard_deviations) {
-  df(standard_deviations)
-}
-# @end degrees_of_freedom
+# @begin TScore
+t_score <- setRefClass("TScore",
+                       fields=list(
+                         x_bar="numeric",
+                         standard_deviations="numeric",
+                         sample_size="numeric",
+                         mu="numeric",
+                         score="numeric",
+                         degrees_of_freedom="numeric"
+                       ),
+                       methods=list(
+                         init_df = function(){
+                           degrees_of_freedom <<- standard_deviations - 1
+                         },
+                         init_score = function(){
+                           score <<- (x_bar - mu)/(standard_deviations/sqrt(sample_size))
+                         }
+                       )
+)
+# @end TScore
 
 #########################
 #       Tests           #
 #########################
 
-all.equal(
-  degrees_of_freedom(
-    5
-  ), 4
-)
+t <- t_score(x_bar=4, standard_deviations=2, sample_size=4, mu=2)
 
-all.equal(
-  round(t_score(
-    x_bar = 4, mu=2, standard_deviations=4, sample_size=2
-  ), 2), 1.41
-)
+# Given A Standard Deviation of 3, DF should be 2.
+t$init_df()
+all.equal(t$degrees_of_freedom, 2)
+
+
+# Given params, determine score.
+t$init_score()
+all.equal(round(t$score, 2), 1.41)
