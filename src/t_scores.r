c48f155046d4834a348359475390458ccf22e6ad
z-weed <1937294+z-weed@users.noreply.github.com>
2023-10-11 13:07:55 -0400

    Update t_scores.r

--- src/t_scores.r
+++ src/t_scores.r
@@ -16,6 +16,15 @@ t_score <- setRefClass("TScore",
                          },
                          init_score = function(){
                            score <<- (x_bar - mu)/(standard_deviations/sqrt(sample_size))
+                         },
+                         x_lt_t = function() {
+                           print(pt(score, degrees_of_freedom()))
+                         },
+                         x_gt_t = function() {
+                           print(1 - pt(score, degrees_of_freedom()))
+                         },
+                         x_lteq_t = function() {
+                           print(qt(score, degrees_of_freedom()))
                          }
                        )
 )
@@ -27,11 +36,27 @@ t_score <- setRefClass("TScore",
 
 t <- t_score(x_bar=4, standard_deviations=2, sample_size=4, mu=2)
 
+# Given 40 Degrees of Freedom, Calculate P(x<T)
+t <- t_score(x_bar=4, standard_deviations=2, sample_size=4, mu=2)
+t$init_df()
+all.equal(t$degrees_of_freedom, 2)
+
 # Given A Standard Deviation of 3, DF should be 2.
 t$init_df()
 all.equal(t$degrees_of_freedom, 2)
 
-
 # Given params, determine score.
 t$init_score()
 all.equal(round(t$score, 2), 1.41)
+
+# Given T 1.05, find P(X < 1.05 with 25 df)
+t <- t_score(score=1.05, degrees_of_freedom=25)
+all.equal(t$degrees_of_freedom, 0.8481186)
+
+# Given T 0.9, find P(X > 1.05 with 25 df)
+t <- t_score(score=0.9, degrees_of_freedom=25)
+all.equal(t$degrees_of_freedom, 0.1883541)
+
+# Given T 0.9, find P(X <= 0.9 with 25 df)
+t <- t_score(score=0.9, degrees_of_freedom=25)
+all.equal(t$degrees_of_freedom, 1.316345)
