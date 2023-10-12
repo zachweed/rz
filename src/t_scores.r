48da0fc1f02c64d7918f33a32dba96d32654969a
z-weed <1937294+z-weed@users.noreply.github.com>
2023-10-12 12:25:54 -0400

    Update t_scores.r

--- src/t_scores.r
+++ src/t_scores.r
@@ -14,7 +14,9 @@ t_score <- setRefClass("TScore",
                          t_critical_value="numeric",
                          t_actual_value="numeric",
                          margin_of_error="numeric",
-                         sample="list"
+                         sample="list",
+                         interval_lower="numeric",
+                         interval_upper="numeric"
                        ),
                        methods=list(
                          init_df = function(){
@@ -34,12 +36,16 @@ t_score <- setRefClass("TScore",
                            alpha <<- 1 - confidence_level
                          },
                          init_t_critical_value = function() {
-                           init_sample_size()
+                           if(sample_size == 0){
+                             init_sample_size()
+                           }
                            init_alpha()
                            t_critical_value <<- alpha/2
                          },
                          init_t_actual_value = function() {
-                           init_df()
+                           if(degrees_of_freedom == 0){
+                             init_df()  
+                           }
                            init_t_critical_value()
                            t_actual_value <<- -qt(t_critical_value, degrees_of_freedom)
                          },
@@ -47,10 +53,13 @@ t_score <- setRefClass("TScore",
                            standard_deviation <<- sd(unlist(sample))
                          },
                          init_margin_of_error = function() {
-                           init_standard_deviation()
-                           init_sample_mean()
+                           if(standard_deviation == 0){
+                            init_standard_deviation()  
+                           }
+                           if(sample_mean == 0){
+                             init_sample_mean()  
+                           }
                            init_t_actual_value()
-                           init_standard_deviation()
                            margin_of_error <<- (t_actual_value * (standard_deviation/sqrt(sample_size)))
                          },
                          x_lt_t = function() {
@@ -64,8 +73,10 @@ t_score <- setRefClass("TScore",
                          },
                          find_confidence_interval = function() {
                            init_margin_of_error()
+                           interval_lower <<- sample_mean - margin_of_error
+                           interval_upper <<- sample_mean + margin_of_error
                            print("true population mean is:")
-                           print(list(sample_mean - margin_of_error, sample_mean + margin_of_error))
+                           list(interval_lower, interval_upper)
                          },
                          find_t_value_for_confidence_interval_with_sample_size_and_mean = function(confidence_interval=0, sample_size=0, sample_mean=0, standard_deviation) {
                            t_critical_value <- qt(confidence_interval, sample_size - 1)
@@ -81,10 +92,10 @@ t_score <- setRefClass("TScore",
 #       Tests           #
 #########################
 
-t <- t_score(sample_mean=4, standard_deviations=2, sample_size=4, mu=2)
+t <- t_score(sample_mean=4, standard_deviation=2, sample_size=4, mu=2)
 
 # Given 40 Degrees of Freedom, Calculate P(x<T)
-t <- t_score(sample_mean=4, standard_deviations=2, sample_size=4, mu=2)
+t <- t_score(sample_mean=4, standard_deviation=2, sample_size=4, mu=2)
 t$init_df()
 all.equal(t$degrees_of_freedom, 2)
 
@@ -107,3 +118,12 @@ all.equal(t$degrees_of_freedom, 0.1883541)
 # Given T 0.9, find P(X <= 0.9 with 25 df)
 t <- t_score(score=0.9, degrees_of_freedom=25)
 all.equal(t$degrees_of_freedom, 1.316345)
+
+# Given A list of babies with chemicals, find mean range for given confidence interval
+t <- t_score(
+  sample=list(90, 145, 147, 160, 116, 100, 159, 151, 156, 126, 137, 83, 156, 94, 121, 144, 123, 114, 139, 99), 
+  confidence_level=0.90
+)
+t$find_confidence_interval()
+all.equal(t$interval_lower, 118.3408)
+all.equal(t$interval_upper, 137.6592)
