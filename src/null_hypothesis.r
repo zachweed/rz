4557bd684111acb6a3b1edde1a0f8af01649a863
Zach Weed <zachweed@gmail.com>
2023-11-04 17:00:09 -0400

    ttd

--- src/null_hypothesis.r
+++ src/null_hypothesis.r
@@ -1,5 +1,5 @@
 # Given means from two different observations,
-#	determine how rare event is by comparing to alpha.
+# determine how rare event is by comparing to alpha.
 # @param original_mean
 # @param different_mean
 # @param sd
@@ -7,14 +7,14 @@
 # @param a
 # @return Whether or not to reject the null-hypothesis.
 p_value <- function(original_mean = 0, different_mean = 0, sd = 0, n = 0, a = 0) {
-	h_o <- original_mean
-	h_a <- different_mean
-	p_value <- pnorm( (different_mean - original_mean) / (sd / sqrt(n) ) )
-	reject <- a > p_value
+  h_o <- original_mean
+  h_a <- different_mean
+  p_value <- pnorm( (different_mean - original_mean) / (sd / sqrt(n) ) )
+  reject <- a > p_value
 
-	if(reject == TRUE){
-		print("Reject Null Hypothesis")
-	} else {
-		print("Fail to Reject Null Hypothesis")
-	}
+  if(reject == TRUE){
+    print("Reject Null Hypothesis")
+  } else {
+    print("Fail to Reject Null Hypothesis")
+  }
 }
