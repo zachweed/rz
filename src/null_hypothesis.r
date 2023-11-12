508d702634630375679e72e87d3b70666aaf5943
Zach Weed <zachweed@gmail.com>
2023-11-12 16:06:24 -0500

    updates

--- src/null_hypothesis.r
+++ src/null_hypothesis.r
@@ -4,12 +4,29 @@ library(methods)
 null_hypothesis_test <- setRefClass("NullHypothesis",
   fields=list(
     rate="numeric", alpha="numeric", gt="numeric", sub_sample_size="numeric", p0="numeric", q0="numeric", mu="numeric", x_bar="numeric", h_o="numeric", h_a="numeric",
-    d_f="numeric", significance_level="numeric", s="numeric", sd="numeric", critical_value="numeric", test_statistic="numeric", tail_number="numeric", sample_size="numeric"
+    d_f="numeric", significance_level="numeric", s="numeric", sd="numeric", critical_value="numeric", test_statistic="numeric", tail_number="numeric", sample_size="numeric",
+    xbar="numeric"
  ),
  methods=list(
   # If given test statistic Z, find p-value.
-  p_value_for_test_statistic_z = function(z) {
-    1 - pnorm(z)
+  # left is always considered less than here.
+  p_value = function() {
+    left <- pnorm(z_stat())
+    right <- 1 - pnorm(z_stat())
+    two_tailed <- 2 * (1 - pnorm(abs(z_stat())))
+    print("if H_0 is true, probability of different mean is:")
+    print(left * 100)
+    if(alpha) { 
+      if(alpha > left) { 
+        print("and alpha is:")
+        print(alpha)
+        print("so reject null-hypothesis") 
+      } else {
+        print("so fail to reject null-hypothesis") 
+      }
+    }
+    print("all data:")
+    c(left, right, two_tailed)
   },
   break_down_data = function() {
     diff <- -(h_a - h_o)
@@ -18,10 +35,15 @@ null_hypothesis_test <- setRefClass("NullHypothesis",
     right_of_test_statistic <- ((1 - test_statistic) * diff)
     list(test_statistic, left_of_test_statistic, right_of_test_statistic)
   },
-  t_test = function(sample, alternative="greater") {
+  # one sample t-test looks for signification deviation from compared mu.
+  one_sample_t_test = function(sample, alternative="greater") {
     print(t.test(sample, mu=mu, alternative=alternative))
     print("Is p-value greater than alpha?")
     print(alpha)
+  },
+  # two sample t-test looks for signification deviation between means
+  two_sample_t_test = function(sample_one, sample_two) {
+    print(t.test(sample_one, sample_two))
   },
   init_cv = function(confidence_level=0.95) {
     if(confidence_level != 0) {
@@ -46,19 +68,11 @@ null_hypothesis_test <- setRefClass("NullHypothesis",
       print("Fail to Reject Null Hypothesis")
     }
   },
-  z_stat = function() { 
-    diff_rate <- rate / 100
-    actual_ratio <- sub_sample_size / sample_size
-    print("p-value:\n")
-    print((1 - pnorm(((actual_ratio) - diff_rate) / sqrt((diff_rate * (1 - diff_rate)) / sample_size))))
-    print("\n")
-    print("alpha:\n")
-    print(alpha)
-    print("\n")
-  },
+  z_stat = function() { (x_bar - mu) / (sd/sqrt(s)) },
   p_value_for = function(z = 0) { 1 - pnorm(z) },
   p_value = function() { 2*(1-pnorm(abs(z_stat()))) },
   # can be negative.
+  # always ((xbar - mu)/s*sqrt(n))
   find_test_statistic = function() { (x_bar - mu)/(sd/sqrt(s)) }
   # distance ( measured mean <-> null mean); in standard errors.
  )
